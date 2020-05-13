432ab58fe50c09c751c5243df7b340df851f488f
Lloyd Rhodes <lloydrhodes@hotmail.co.uk>
2020-05-13 09:44:25 +0100

    test

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+// import { OnfidoService } from '../../services/onfido-service/onfido.service';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
-export class HelloIonicPage {
-  constructor() {
 
+export class HelloIonicPage {
+  applicantId: string = '63fbbb62-f225-4ec2-b6ec-7209589c43f9'
+  token: string = 'sdk_sandbox.UFEcqDXJSAw.NKCR1u-4UUTzlkevWsECJXAlLU-vaVvz'
+
+
+  options: any;
+  constructor(
+    // private onfido: OnfidoService
+  ) {
+    var initobject = {
+      token:  '',
+      applicant_id: '',
+      flow_steps: [ 'welcome', 'document', 'face', 'final'],
+  }
+  }
+
+  handleOnfido() {
+  
+    try {
+      console.log(1)
+      window['cordova'].plugins.Onfido.init((res) => {
+        console.log(res)
+        this.onComplete(res)
+      }, this.options)
+      console.log(2)
+
+    } catch (error) {
+      console.log('ERROR', error)
+    }
+     
+   
+
+  
+}
+onComplete(completeResponse) {
+  console.log('in on complete', completeResponse)
   }
 }
+
